cb4acabac14e02710174ebcacbbafda9c6a311d0
Matthew Willcockson <matthew@willcockson.family>
2020-04-12 14:44:41 -0500

    (bugfix/feature)Doesn't automatically clobber; fonts sized correctly

--- text2png.py
+++ text2png.py
@@ -1,16 +1,17 @@
 """Takes a text file and uses Pillow to generate PNGs of those lines of text"""
+import logging
 import math
 import sys
-from pathlib import Path
-from re import compile as re_compile
-from typing import List, NewType, Optional, Tuple, Union, NamedTuple
 from argparse import ArgumentParser, ArgumentTypeError, Namespace
 from functools import reduce
 from numbers import Number
+from pathlib import Path
+from re import compile as re_compile
+from typing import List, NamedTuple, NewType, Optional, Tuple, Union
 from warnings import warn
 
 from matplotlib.font_manager import FontProperties, findfont
-from PIL import Image, ImageDraw, ImageFont, ImageColor
+from PIL import Image, ImageColor, ImageDraw, ImageFont
 from PIL.ImageColor import getrgb
 
 Size = NamedTuple("Size", width=Number, height=Number)
@@ -22,6 +23,7 @@ default_canvas_size = Size(1024, 1024)
 default_padding = 0.10
 default_background = "white"
 default_text_color = "black"
+default_log_level = logging.WARNING
 
 
 # Regexes
@@ -58,7 +60,7 @@ def center_text_position(
     leftover_width = canvas_size.width - text_size.width - (padding_width * 2)
     leftover_height = canvas_size.height - text_size.height - (padding_height * 2)
     if leftover_height < 0 or leftover_width < 0:
-        raise ValueError("Calculation error: can't size text")
+        raise ValueError("Calculation error: text too big for canvas")
 
     x = math.floor((leftover_width / 2) + padding_width)
     y = math.floor((leftover_height / 2) + padding_height)
@@ -91,17 +93,50 @@ def comment_or_blank(string: str) -> bool:
     return comment_re.search(string) or blank_re.search(string) or string == ""
 
 
-def get_lines(text_file: Union[Path, str]) -> List[str]:
+def check_collisions(lines: List[str], directory: Union[Path, str]) -> dict:
+    dir_path = Path(directory)
+    if not (dir_path.is_dir() and dir_path.exists()):
+        raise ValueError(f"'{dir_path}' must be a directory")
+    dir_contents = list(dir_path.iterdir())
+    non_files = filter(lambda f: not f.is_file(), dir_contents)
+    colliding_names = [line for line in lines if (line + ".png") in non_files]
+    for line in colliding_names:
+        logging.error(
+            f"'{line}.png' can't be created because there's something that's not a picture in the directory '{directory}' that already has that name"
+        )
+    if colliding_names:
+        colliding_list = "\n".join(colliding_names)
+        raise FileExistsError(
+            f"These are names of files that would be created in '{directory}', but can't:\n{colliding_list}"
+        )
+    return {file.stem: file for file in dir_contents}
+
+
+def get_lines(text_file: Union[Path, str], directory: Path, clobber: bool) -> List[str]:
     """Returns a list of the lines from a file that aren't empty, and don't
     have a '#' as the first non-whitespace character"""
     text_path = Path(text_file)
     if not text_path.is_file():
         raise Exception(f"'{text_path}' is not a file")
-    return [
+    lines = [
         line
         for line in text_path.read_text(encoding="utf-8").splitlines()
         if not comment_or_blank(line)
     ]
+    # Check to see if any lines clash with existing directory or non-file names
+    dir_contents = check_collisions(lines=lines, directory=directory)
+    # If we're clobbering, don't filter; otherwise, remove any lines that already have pictures generated
+    if clobber:
+        filtered_lines = lines
+    else:
+        unique_lines = set(lines)
+        dir_set = set(dir_contents.keys())
+        for line in unique_lines & dir_set:
+            logging.info(f"Not clobbering '{line}.png'")
+
+        filtered_lines = list(unique_lines - dir_set)
+
+    return filtered_lines
 
 
 def parse_size(size: str) -> Size:
@@ -114,53 +149,77 @@ def parse_size(size: str) -> Size:
 
 
 def get_max_text_size(lines: List[str], font: str) -> Size:
-    """Return the approximate size in pixels the largest dimensions of any line of text would be if the font size was 1pt"""
+    """Return the approximate size in pixels of the smallest bounding box that can enclose every line of text at a font size of 1pt"""
     font_file = findfont(font)
-    font_image = ImageFont.truetype(font_file, size=100)
+    font_image = ImageFont.truetype(font_file, size=1000)
     text_sizes = map(font_image.getsize, lines)
-    tuple_max = lambda a, b: (max(a[0], b[0]), max(a[1], b[1]))
-    max_size_at_100 = reduce(tuple_max, text_sizes)
-    return Size(max_size_at_100[0] / 100, max_size_at_100[1] / 100)
+    tuple_max = lambda a, b: Size(max(a[0], b[0]), max(a[1], b[1]))
+    max_size_at_1000 = reduce(tuple_max, text_sizes, Size(0, 0))
+    return Size(max_size_at_1000.width / 1000, max_size_at_1000.height / 1000)
 
 
 def get_font(
-    lines: List[str], canvas_size: Size, padding: int, font_name: Optional[str] = None
+    lines: List[str],
+    canvas_size: Size,
+    padding: int,
+    font_name: Optional[str] = default_font,
 ) -> ImageFont:
     max_text_size = get_max_text_size(lines=lines, font=font_name)
+    if max_text_size.height <= 0 or max_text_size.width <= 0:
+        logging.warn("No text will be drawn")
+        font_file = findfont(font_name)
+        return ImageFont.truetype(font_file, size=1)
+
     usable_width = canvas_size.width - padding * canvas_size.width
     usable_height = canvas_size.height - padding * canvas_size.height
     if usable_height < 10:
-        warn(f"The text will have a height of {usable_height}px")
+        logging.warn(f"The text will have a height of {usable_height}px")
     if usable_width < 10:
-        warn(f"The text will have a width of {usable_width}px")
+        logging.warn(f"The text will have a width of {usable_width}px")
     # By what scale factor can I multiply a rectangle with max_text_size dimensions to completely fill my useable area?
     # If I made the height as large as the useable height, would the width be larger than the useable width?
-    scale_factor = usable_height / max_text_size.height
-    if scale_factor * max_text_size.width > usable_width:
-        font_size = math.floor(usable_width / max_text_size.width)
-    else:
-        font_size = math.floor(scale_factor)
+    max_font_size_height = usable_height / max_text_size.height
+    max_font_size_width = usable_width / max_text_size.width
+    # Which scale factor won't overflow the useable area?
+    # NOTE: Subtracting 1 because getsize(text) rounds up
+    font_size = math.floor(min(max_font_size_height, max_font_size_width)) - 1
     font_file = findfont(font_name)
     font_image = ImageFont.truetype(font_file, size=font_size)
     return font_image
 
 
-def main(
-    text_file: Path,
-    output_dir: Path = default_output_dir,
-    font: str = default_font,
-    canvas_size: Size = default_canvas_size,
-    padding: float = default_padding,
-    background: str = default_background,
-    text_color: str = default_text_color,
-) -> None:
-    background_color = getrgb(background)
+def setup_logging(args: Optional[Namespace] = None) -> None:
+    if not args:
+        return logging.basicConfig(format="%(message)s", level=logging.WARNING)
 
-    fill_color = getrgb(text_color)
+    return logging.basicConfig(format="%(message)s", level=args.log)
 
-    lines = get_lines(text_file)
 
-    image_font = get_font(lines, canvas_size, padding, font)
+def main(args: Namespace,) -> None:
+    setup_logging(args)
+
+    font: str = args.font
+
+    canvas_size: Size = args.size
+
+    padding: float = args.padding
+
+    text_color: str = args.text_color
+
+    background_color = getrgb(color=args.background)
+
+    fill_color = getrgb(color=args.text_color)
+
+    lines = get_lines(
+        text_file=args.file, directory=args.output_dir, clobber=args.clobber
+    )
+    if not lines:
+        logging.info("No pictures will be generated")
+        return
+
+    image_font = get_font(
+        lines=lines, canvas_size=canvas_size, padding=padding, font_name=font
+    )
 
     for text in lines:
         generate_png(
@@ -171,19 +230,45 @@ def main(
             background=background_color,
             fill_color=fill_color,
         ).save(
-            fp=assign_path(text, output_dir), format="PNG",
+            fp=assign_path(text=text, dir=args.output_dir), format="PNG",
         )
 
 
 if __name__ == "__main__":
+    # setup_logging()
+
     parser = ArgumentParser(description="Generates images of characters")
 
     def str_to_dir(path: str) -> Path:
-        path = Path(path)
-        if path.is_dir():
-            return path
+        dir = Path(path)
+        if dir.is_dir():
+            return dir
         else:
-            raise ArgumentTypeError("PATH needs to be a directory that exists")
+            raise ArgumentTypeError(f"{dir} needs to be a directory that exists")
+
+    def create_if_absent(path: str) -> Path:
+        dir = Path(path)
+        if not dir.exists():
+            dir.mkdir()
+
+        if not dir.is_dir():
+            raise ArgumentTypeError(f"'{dir}' must be a directory")
+
+        return dir
+
+    def parse_log_level(level: str) -> str:
+        levels = {
+            "critical": logging.CRITICAL,
+            "error": logging.ERROR,
+            "warning": logging.WARNING,
+            "info": logging.INFO,
+            "debug": logging.DEBUG,
+        }
+        if not level.lower() in levels:
+            raise ArgumentTypeError(
+                f"'{level}' isn't one of: {' '.join(levels.keys())}"
+            )
+        return levels[level.lower()]
 
     parser.add_argument(
         "-f",
@@ -195,7 +280,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-d",
         "--output-dir",
-        type=str_to_dir,
+        type=create_if_absent,
         default=default_output_dir,
         help="Directory in which to output pictures",
     )
@@ -218,14 +303,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "--text-color", default=default_text_color, help="Color to use for the text"
     )
-
-    args = parser.parse_args()
-    main(
-        text_file=args.file,
-        output_dir=args.output_dir,
-        font=args.font,
-        canvas_size=args.size,
-        padding=args.padding,
-        background=args.background,
-        text_color=args.text_color,
+    parser.add_argument(
+        "--clobber",
+        action="store_true",
+        help="If passed, will overwrite existing files; otherwise, nothing is clobbered",
     )
+    parser.add_argument(
+        "--log",
+        default=default_log_level,
+        type=parse_log_level,
+        help="Verbosity/log level",
+    )
+
+    main(args=parser.parse_args())
